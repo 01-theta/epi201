ca48154a69b3b262b7baa97cc4410ae63174f49f
lab01 <38508669+01-theta@users.noreply.github.com>
2022-10-28 08:58:39 -0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,40 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="index.html">EPI201 Home</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="index.html#content">Content</a></li>\
+              <li><a href="index.html#schedule">Schedule</a></li>\
+              <li><a href="1info.html">Course Info</a></li>\
+              <li><a href="1project.html">Project</a></li>\
+              <li><a href="1data.html">Data</a></li>\
+              <li><a href="1office_hours.html">Office Hours</a></li>\
+              <li><a href="1faq.html">FAQ</a></li>\
+              <li><a href="1https://edstem.org/us/courses/4942/discussion/">{note}</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="http://cs.washington.edu/">\
+    <img src="images/uw_logo.png" class="logo-left">\
+  </a>\
+  <h1>EPI201: Outbreak Investigation & Response</h1>\
+  <h3>UW / Spring 2022</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
